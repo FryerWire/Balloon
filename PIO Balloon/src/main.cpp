e589f73a73cf34931714239a553c2dd2933d3c54
FryerWire <maxwell.seery@gmail.com>
2025-04-07 23:39:29 -0500

    Testing the merge from the old code

--- PIO Balloon/src/main.cpp
+++ PIO Balloon/src/main.cpp
@@ -1,18 +1,11 @@
+// /*
+// main.cpp
+// Start Date : 4/7/2025
+// Modified   : 4/7/2025
+// */
+
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+int a = 10;       // Integer
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-}
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
