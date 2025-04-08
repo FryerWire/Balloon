9b416755b429b9f4381d19a6072788d163e84aa5
FryerWire <maxwell.seery@gmail.com>
2025-04-08 13:29:39 -0500

    Beginning to learn the C++ library

--- PIO Balloon/src/main.cpp
+++ PIO Balloon/src/main.cpp
@@ -1,11 +1,17 @@
-// /*
-// main.cpp
-// Start Date : 4/7/2025
-// Modified   : 4/7/2025
-// */
+
+/*
+main.cpp
+Start Date : 4/7/2025
+Modified   : 4/7/2025
+*/
+
 
 #include <Arduino.h>
+// #include other libraries
 
-int a = 10;       // Integer
 
+// Balloon Prgram Consts ==========================================================================
+const int ledRedPin = 13;   // UPDATE THE PORTS
+const int ledGreenPin = 12; // UPDATE THE PORTS
+const int ledBluePin = 11;  // UPDATE THE PORTS
 
